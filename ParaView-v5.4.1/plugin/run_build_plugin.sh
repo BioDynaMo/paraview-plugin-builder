d97f938331b2f2bc01a449148ef593b686ad48cd
Mathieu Westphal <mathieu.westphal@kitware.com>
2017-09-15 16:50:15 +0200

    Adding ParaView-v5.4.1 support

---
+++ ParaView-v5.4.1/plugin/run_build_plugin.sh
@@ -0,0 +1,12 @@
+docker create --name=paraview paraview:5.4.1 /bin/sh -c "while true; do echo hello world; sleep 1; done"
+docker start paraview
+docker cp ../docker/build_plugin.sh paraview:/home/buildslave/
+docker cp plugin.cmake paraview:/home/buildslave/
+docker cp plugin.tgz paraview:/home/buildslave/
+echo "Building Plugin"
+docker exec paraview scl enable devtoolset-4 -- sh /home/buildslave/build_plugin.sh
+docker cp paraview:/home/buildslave/misc/code/plugin/build ./
+docker stop paraview
+docker rm paraview
+cp ./build/lib*.so ./
+rm -rf ./build
